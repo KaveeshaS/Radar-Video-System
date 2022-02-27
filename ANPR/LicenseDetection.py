8c6d5532c95067cf0f0cfa2aac1f817d28a49685
Kaveesha Siribaddana <kavee360@gmail.com>
2022-02-27 14:22:49 -0500

    added OCR filtering and result saving function

--- ANPR/LicenseDetection.py
+++ ANPR/LicenseDetection.py
@@ -62,6 +62,64 @@ def detect_fn(image):
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import easyocr
+import csv
+from datetime import datetime
+import shortuuid
+
+detection_threshold = 0.7
+region_threshold = 0.6
+
+def filter_text(region, ocr_result, region_threshold):
+    rectangle_size = region.shape[0] * region.shape[1]
+
+    plate = []
+    for result in ocr_result:
+        length = np.sum(np.subtract(result[0][1], result[0][0]))
+        height = np.sum(np.subtract(result[0][2], result[0][1]))
+
+        if length * height / rectangle_size > region_threshold:
+            plate.append(result[1])
+    return plate
+
+def ocr_it(image, detections, detection_threshold, region_threshold):
+    # Scores, boxes and classes above threshold
+    scores = list(filter(lambda x: x > detection_threshold, detections['detection_scores']))
+    boxes = detections['detection_boxes'][:len(scores)]
+    classes = detections['detection_classes'][:len(scores)]
+
+    # Full image dimensions
+    width = image.shape[1]
+    height = image.shape[0]
+
+    # Apply ROI filtering and OCR
+    for idx, box in enumerate(boxes):
+        roi = box * [height, width, height, width]
+        region = image[int(roi[0]):int(roi[2]), int(roi[1]):int(roi[3])]
+        reader = easyocr.Reader(['en'])
+        ocr_result = reader.readtext(region)
+
+        text = filter_text(region, ocr_result, region_threshold)
+
+        plt.imshow(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
+        plt.show()
+        return text, region
+
+def save_results(text, region, csv_filename, folder_path):
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+    uuid = shortuuid.uuid()
+
+    fileName = current_time + "-" + uuid
+
+    img_name = '{}.jpg'.format(fileName)
+
+    cv2.imwrite(os.path.join(folder_path, img_name), region)
+
+    with open(csv_filename, mode='a', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([img_name, text])
+
 
 category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
 
@@ -97,6 +155,12 @@ while cap.isOpened():
         max_boxes_to_draw=5,
         min_score_thresh=.8,
         agnostic_mode=False)
+
+    try:
+        text, region = ocr_it(image_np_with_detections, detections, detection_threshold, region_threshold)
+        save_results(text, region, 'realtimeresults.csv', 'Detection_Images')
+    except:
+        pass
 
     cv2.imshow('object detection', cv2.resize(image_np_with_detections, (800, 600)))
 
