d5dfb2eec0371260d647cdddeecc12ca008569a5
Kaveesha Siribaddana <kavee360@gmail.com>
2022-04-07 19:48:25 -0400

    added ocr

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import re
 import cv2
 from tflite_runtime.interpreter import Interpreter
 import numpy as np
+from matplotlib import pyplot as plt
+import easyocr
+import csv
+from datetime import datetime
+import shortuuid
+import os
 
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
@@ -52,6 +58,59 @@ def detect_objects(interpreter, image, threshold):
             results.append(result)
     return results
 
+detection_threshold = 0.7
+region_threshold = 0.6
+
+def filter_text(region, ocr_result, region_threshold):
+    rectangle_size = region.shape[0] * region.shape[1]
+
+    plate = []
+    for result in ocr_result:
+        length = np.sum(np.subtract(result[0][1], result[0][0]))
+        height = np.sum(np.subtract(result[0][2], result[0][1]))
+
+        if length * height / rectangle_size > region_threshold:
+            plate.append(result[1])
+    return plate
+
+def ocr_it(image, detections, detection_threshold, region_threshold):
+    # Scores, boxes and classes above threshold
+    scores = list(filter(lambda x: x > detection_threshold, detections['bounding_box']))
+    boxes = detections['bounding_box'][:len(scores)]
+    classes = detections['class_id'][:len(scores)]
+
+    # Full image dimensions
+    width = image.shape[1]
+    height = image.shape[0]
+
+    # Apply ROI filtering and OCR
+    for idx, box in enumerate(boxes):
+        roi = box * [height, width, height, width]
+        region = image[int(roi[0]):int(roi[2]), int(roi[1]):int(roi[3])]
+        reader = easyocr.Reader(['en'])
+        ocr_result = reader.readtext(region)
+
+        text = filter_text(region, ocr_result, region_threshold)
+
+        plt.imshow(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
+        plt.show()
+        return text, region
+
+def save_results(text, region, csv_filename, folder_path):
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+    uuid = shortuuid.uuid()
+
+    fileName = current_time + "-" + uuid
+
+
+    img_name = '{}.jpg'.format(fileName)
+
+    cv2.imwrite(os.path.join(folder_path, img_name), region)
+
+    with open(csv_filename, mode='a', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([img_name, text])
 
 def main():
     labels = load_labels()
@@ -60,13 +119,22 @@ def main():
     _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
 
     cap = cv2.VideoCapture(1)
+
     while cap.isOpened():
         ret, frame = cap.read()
         img = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (320, 320))
         res = detect_objects(interpreter, img, 0.8)
-        print(res)
 
+        try:
+            text, region = ocr_it(img, res, detection_threshold, region_threshold)
+            save_results(text, region, 'realtimeresults.csv', 'Detection_Images')
+        except:
+            pass
+
+        '''
+        print(res)
         for result in res:
+        
             ymin, xmin, ymax, xmax = result['bounding_box']
             xmin = int(max(1, xmin * CAMERA_WIDTH))
             xmax = int(min(CAMERA_WIDTH, xmax * CAMERA_WIDTH))
@@ -82,6 +150,7 @@ def main():
         if cv2.waitKey(10) & 0xFF == ord('q'):
             cap.release()
             cv2.destroyAllWindows()
+        '''
 
 
 if __name__ == "__main__":
