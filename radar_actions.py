ace069477de2c6413a005e21ec51a9035c13c18f
YelizavetaGad <99437548+YelizavetaGad@users.noreply.github.com>
2022-04-13 18:30:19 -0400

    added radar-action

---
+++ radar_actions.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def on_target_acquired(recent_speed):
+    now = datetime.now()
+    print(f'on_target_acquired called at {now.strftime("%d/%m/%Y %H:%M:%S")}')
+
+def on_target_accelerating(recent_speed):
+    now = datetime.now()
+    print(f'on_target_accelerating called at {now.strftime("%d/%m/%Y %H:%M:%S")}')
+
+def on_target_decelerating(recent_speed):
+    now = datetime.now()
+    print(f'on_target_decelerating called at {now.strftime("%d/%m/%Y %H:%M:%S")}')
+
+def on_target_lost():
+    now = datetime.now()
+    print(f'on_target_lost called at {now.strftime("%d/%m/%Y %H:%M:%S")}')
+
+def on_idle_notice_interval():
+    now = datetime.now()
+    print(f'on_target_lost called at {now.strftime("%d/%m/%Y %H:%M:%S")}')
