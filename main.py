102aa1366f0a5c686ee52b7fd9c095cbbad694f5
Kaveesha Siribaddana <kavee360@gmail.com>
2022-04-13 16:09:03 -0400

    test

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def filter_text(region, ocr_result, region_threshold):
 
 def ocr_it(image, detections, detection_threshold, region_threshold):
     # Scores, boxes and classes above threshold
-    scores = int(list(filter(lambda x: x > detection_threshold, detections['bounding_box'])))
+    scores = list(filter(lambda x: x > detection_threshold, detections['bounding_box'])).astype(np.int64)
     boxes = detections[0]['bounding_box'][:len(scores)]
     #classes = detections[0]['class_id'][:len(scores)]
 
